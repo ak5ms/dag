ca535e17cdf8cfe7e83a41cb07c8c21518fa0d7b
ak5ms <109175847+ak5ms@users.noreply.github.com>
2026-07-31 16:06:57 -0500

    Expose cpp_stream Python API

---
+++ src/trading_dsl_engine/cpp_stream/python/__init__.py
@@ -0,0 +1,4 @@
+from trading_dsl_engine.cpp_stream.python.compile import compile_formula
+from trading_dsl_engine.cpp_stream.python.runtime import CppStreamRuntime, RunResult
+
+__all__ = ["compile_formula", "CppStreamRuntime", "RunResult"]
