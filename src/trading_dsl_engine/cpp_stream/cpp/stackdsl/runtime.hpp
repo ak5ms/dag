ccdbe14736c0f743dbcee5aa02501a347f857cf2
ak5ms <109175847+ak5ms@users.noreply.github.com>
2026-07-31 16:10:38 -0500

    Add cpp_stream runtime umbrella header

---
+++ src/trading_dsl_engine/cpp_stream/cpp/stackdsl/runtime.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "stackdsl/utils.hpp"
+#include "stackdsl/engine.hpp"
+#include "stackdsl/io.hpp"
+#include "stackdsl/ops/naryop.hpp"
+#include "stackdsl/ops/cumsum.hpp"
+#include "stackdsl/ops/ewm.hpp"
+#include "stackdsl/ops/groupby.hpp"
