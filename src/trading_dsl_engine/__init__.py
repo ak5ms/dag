606d3b8abce8d822bb08fecaf902384d7a8a8e26
ak5ms <109175847+ak5ms@users.noreply.github.com>
2026-07-31 19:24:38 -0500

    Export group key descriptor API

--- src/trading_dsl_engine/__init__.py
+++ src/trading_dsl_engine/__init__.py
@@ -75,8 +75,7 @@ from trading_dsl_engine.base.dsl import (
     arctan,
     pow,
 )
-
-
+from trading_dsl_engine.base.keys import Key, key
 from trading_dsl_engine.base.terminals import (
     feature_names_with_tags,
     futures_field_metadata,
@@ -98,6 +97,8 @@ __all__ = [
     "call",
     "op",
     "var",
+    "Key",
+    "key",
     "add",
     "sub",
     "mul",
