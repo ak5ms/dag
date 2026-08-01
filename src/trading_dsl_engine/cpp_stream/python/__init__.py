58973749eb369564f0913611777ca3fa89d6276e
ak5ms <109175847+ak5ms@users.noreply.github.com>
2026-07-31 19:36:43 -0500

    Export typed NumPy mmap compilation API

--- src/trading_dsl_engine/cpp_stream/python/__init__.py
+++ src/trading_dsl_engine/cpp_stream/python/__init__.py
@@ -1,4 +1,23 @@
-from trading_dsl_engine.cpp_stream.python.compile import compile_formula
+from trading_dsl_engine.cpp_stream.python.compile import compile_formula, compile_npy_formula
+from trading_dsl_engine.cpp_stream.python.npy import (
+    InputTypeSpec,
+    NpyArrayInfo,
+    NpyMMap,
+    inspect_npy,
+    inspect_npy_mapping,
+    mmap_npy,
+)
 from trading_dsl_engine.cpp_stream.python.runtime import CppStreamRuntime, RunResult
 
-__all__ = ["compile_formula", "CppStreamRuntime", "RunResult"]
+__all__ = [
+    "compile_formula",
+    "compile_npy_formula",
+    "CppStreamRuntime",
+    "RunResult",
+    "InputTypeSpec",
+    "NpyArrayInfo",
+    "NpyMMap",
+    "inspect_npy",
+    "inspect_npy_mapping",
+    "mmap_npy",
+]
