323d38ecc9fcc793004f8461be46e6cd7f12f199
ak5ms <109175847+ak5ms@users.noreply.github.com>
2026-08-01 16:03:02 -0500

    Expose explicit nonfinite literals

--- src/trading_dsl_engine/cpp_stream/cpp/stackdsl/runtime.hpp
+++ src/trading_dsl_engine/cpp_stream/cpp/stackdsl/runtime.hpp
@@ -3,6 +3,7 @@
 #include "stackdsl/utils.hpp"
 #include "stackdsl/engine.hpp"
 #include "stackdsl/io.hpp"
+#include "stackdsl/ops/literal.hpp"
 #include "stackdsl/ops/naryop.hpp"
 #include "stackdsl/ops/cat.hpp"
 #include "stackdsl/ops/cumsum.hpp"
