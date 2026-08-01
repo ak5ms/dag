df9b39307dd9aaf5fda9022bed8704fbb9deea7a
ak5ms <109175847+ak5ms@users.noreply.github.com>
2026-08-01 10:21:59 -0500

    Expose cat and Ridge native nodes

--- src/trading_dsl_engine/cpp_stream/cpp/stackdsl/runtime.hpp
+++ src/trading_dsl_engine/cpp_stream/cpp/stackdsl/runtime.hpp
@@ -4,6 +4,8 @@
 #include "stackdsl/engine.hpp"
 #include "stackdsl/io.hpp"
 #include "stackdsl/ops/naryop.hpp"
+#include "stackdsl/ops/cat.hpp"
 #include "stackdsl/ops/cumsum.hpp"
 #include "stackdsl/ops/ewm.hpp"
+#include "stackdsl/ops/ridge.hpp"
 #include "stackdsl/ops/groupby.hpp"
